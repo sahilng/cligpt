f785381072103ff808d3da23a5ec402ecd01db2d
sahilng <sahilng1997@gmail.com>
2025-10-19 03:53:39 -0400

    update

--- shortcut.sh
+++ shortcut.sh
@@ -4,6 +4,18 @@ set -e  # exit on any error
 # Determine the directory of this script (portable!)
 BASE_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-shell_rc="${ZDOTDIR:-$HOME}/.zshrc"
-[ -n "$BASH_VERSION" ] && shell_rc="$HOME/.bashrc"
-echo "alias '?'=\"$BASE_DIR/run.sh\"" >> "$shell_rc" && echo "Added to $shell_rc"
+# Detect user's login shell
+case "$SHELL" in
+  */zsh)
+    shell_rc="${ZDOTDIR:-$HOME}/.zshrc"
+    ;;
+  */bash)
+    shell_rc="$HOME/.bashrc"
+    ;;
+  *)
+    shell_rc="$HOME/.profile"
+    ;;
+esac
+
+echo "alias '?'=\"$BASE_DIR/run.sh\"" >> "$shell_rc"
+echo "Added to $shell_rc"
